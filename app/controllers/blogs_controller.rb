6998817628b1d316335e4a12790c5a708a508ec8
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-06-23 20:04:15 -0500

    change header color

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,38 +17,30 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new params.require(:blog).permit(:title, :body, :id)
+    @blog = Blog.new params.require(:blog).permit(:title, :body, :id, :published_on, :image)
 
     if @blog.save
-       redirect_to root_path
+       redirect_to root_path, notice: "You created a new blog post!"
     else
        render :new
      end
+
   end
 
   def update
     @blog = Blog.find params[:id]
-    if @blog.update params.require(:blog).permit(:title,:body, :id)
-      redirect_to blog_path(@blog)
+    if @blog.update params.require(:blog).permit(:title,:body, :id, :published_on, :image)
+      redirect_to blog_path(@blog), notice: "You updated a blog post!"
     else
       render :edit
     end
+  end
 
     def delete
       @blog = Blog.find params[:id]
       @blog.destroy
-      redirect_to root_path
+      redirect_to root_path, notice: "You deleted a blog post!"
     end
-
-
-
-  end
-
-
-
-
-
-
 
 
 
